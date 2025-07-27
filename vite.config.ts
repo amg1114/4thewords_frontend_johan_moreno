bdcf392793d5a9a5fd5825090a85a7cc9533a1a0
Alejandro Moreno <alejandromogil@gmail.com>
2025-07-27 18:24:57 -0500

    feat: TailwindCSS installation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 3000,
+      cors: true,
+      proxy: {
+        "/api": {
+          changeOrigin: true,
+          target: env.VITE_API_URL   
+        },
+      },
+    },
+  };
+});
