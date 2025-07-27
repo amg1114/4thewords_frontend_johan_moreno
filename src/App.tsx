353016670be2e86150b566358de853fd3ecd6bea
Alejandro Moreno <alejandromogil@gmail.com>
2025-07-27 18:35:29 -0500

    feat: implement routing in App component and add path aliases in Vite config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
 function App() {
-  return (
-    <>
-      <div>
-        <h1 className="text-amber-300">Works</h1>
-      </div>
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <h1 className="text-amber-300">Layout</h1>,
+    },
+    {},
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
