f1764fc18a8116099e502f556128cf6b6f2f184e
Alejandro Moreno <alejandromogil@gmail.com>
2025-07-27 18:27:57 -0500

    feat: Prettier installation

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react(), tailwindcss()],
     server: {
       port: 3000,
       cors: true,
       proxy: {
-        "/api": {
+        '/api': {
           changeOrigin: true,
-          target: env.VITE_API_URL   
+          target: env.VITE_API_URL,
         },
       },
     },
