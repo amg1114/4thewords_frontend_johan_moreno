5bfa4b8ff741479215bd15ec82271aa912407ebd
Alejandro Moreno <alejandromogil@gmail.com>
2025-07-27 21:20:44 -0500

    feat(auth): implement user login authentication flow

---
+++ src/pages/auth/login/LoginPage.tsx
@@ -0,0 +1,24 @@
+import StyledInput from '@components/forms/StyledInput';
+import StyledButton from '@components/StyledButton';
+import StyledLink from '@components/StyledLink';
+import useLogin from './_hooks/useLogin';
+
+export function LoginPage() {
+  const { isLoading, errors, handleSubmit, register } = useLogin();
+  return (
+    <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
+      <h1 className="text-center text-2xl font-semibold">Login</h1>
+      {isLoading && <p className="text-center text-gray-500">Loading...</p>}
+
+      <StyledInput type="text" {...register('email')} errors={errors.email?.message} label="E-mail" />
+      <StyledInput type="password" {...register('password')} errors={errors.password?.message} label="Password" />
+
+      <button type="submit">Hola</button>
+
+      <StyledButton type="submit" variant="primary">
+        Login
+      </StyledButton>
+      <StyledLink to="/register">You don't have an account?</StyledLink>
+    </form>
+  );
+}
